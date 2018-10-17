229549d04eec726a9ba4b3dfde0fab5f77244fbf
kbpsystem777 <koleen.bp@outlook.com>
2018-10-18 04:19:56 +0800

    pushing as build

--- src/FrontEnd/Layout.js
+++ src/FrontEnd/Layout.js
@@ -4,7 +4,7 @@ import { USER_CONNECTED, LOGOUT } from '../Events'
 import LoginForm from './LoginForm'
 import ChatContainer from './chat/ChatContainer'
 
-const socketUrl = "https://stoic-jones-36cb2d.netlify.com/"
+const socketUrl = "/"
 export default class Layout extends Component {
 	
 	constructor(props) {
--- src/Server/Server.js
+++ src/Server/Server.js
@@ -1,12 +1,15 @@
-const app = require('http').createServer()
-const io = module.exports.io = require('socket.io')(app)
+const express = require('express');
+const app = express()
+const server = require('http').Server(app);
+
+const io = module.exports.io = require('socket.io')(server)
 
 const PORT = process.env.PORT || 3373
 
 const SocketManager = require('./SocketManager')
-
+app.use(express.static(__dirname + '/../../build'))
 io.on('connection', SocketManager)
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`App is running on http://localhost:${PORT}`);
 })
